ce2cc6c996d39cde9adc1fc09a3ac482fd503ecd
zhucj <zhucj@servyou.com.cn>
2022-04-13 16:50:57 +0800

    CrQuestionListWindowFactory

---
+++ src/main/java/com/janita/plugin/cr/window/CrQuestionListWindowFactory.java
@@ -0,0 +1,27 @@
+package com.janita.plugin.cr.window;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+/**
+ * CrQuestionListWindowFactory
+ *
+ * @author zhucj
+ * @since 20220324
+ */
+public class CrQuestionListWindowFactory implements ToolWindowFactory {
+
+    @Override
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        CrQuestionListWindow window = new CrQuestionListWindow();
+        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+        Content content = contentFactory.createContent((JComponent) window.getContentPane(), "", false);
+        toolWindow.getContentManager().addContent(content);
+    }
+}
