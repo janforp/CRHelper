60fc62ab9d682a3055a9c8afaea77079c198d65a
zhucj <zhucj@servyou.com.cn>
2022-04-14 21:49:50 +0800

    模版

--- src/main/java/com/janita/plugin/cr/util/CrQuestionUtils.java
+++ src/main/java/com/janita/plugin/cr/util/CrQuestionUtils.java
@@ -75,19 +75,18 @@ public class CrQuestionUtils {
         List<CrQuestion> questionList = new ArrayList<>(crQuestionList.size());
         for (CrQuestion question : crQuestionList) {
             CrQuestion clone = new CrQuestion();
-            clone.setProjectName(StringUtils.defaultIfBlank(question.getProjectName(), StringUtils.EMPTY));
-            clone.setGitBranchName(StringUtils.defaultIfBlank(question.getGitBranchName(), StringUtils.EMPTY));
-            clone.setClassName(StringUtils.defaultIfBlank(question.getClassName(), StringUtils.EMPTY));
+            clone.setProjectName(StringUtils.defaultIfBlank(question.getProjectName(), "无"));
+            clone.setGitBranchName(StringUtils.defaultIfBlank(question.getGitBranchName(), "无"));
+            clone.setClassName(StringUtils.defaultIfBlank(question.getClassName(), "无"));
             clone.setLineFrom(ObjectUtils.defaultIfNull(question.getLineFrom(), 0));
             clone.setLineTo(ObjectUtils.defaultIfNull(question.getLineTo(), 0));
-            clone.setType(StringUtils.defaultIfBlank(question.getType(), StringUtils.EMPTY));
-            clone.setToAccount(StringUtils.defaultIfBlank(question.getToAccount(), StringUtils.EMPTY));
-            clone.setLevel(StringUtils.defaultIfBlank(question.getLevel(), StringUtils.EMPTY));
-            clone.setState(StringUtils.defaultIfBlank(question.getState(), StringUtils.EMPTY));
-            clone.setQuestionCode(setCodeMark(StringUtils.defaultIfBlank(question.getQuestionCode(), StringUtils.EMPTY)));
-            clone.setBetterCode(setCodeMark(StringUtils.defaultIfBlank(question.getBetterCode(), StringUtils.EMPTY)));
-            clone.setDesc(StringUtils.defaultIfBlank(question.getDesc(), StringUtils.EMPTY));
-
+            clone.setType(StringUtils.defaultIfBlank(question.getType(), "无"));
+            clone.setToAccount(StringUtils.defaultIfBlank(question.getToAccount(), "无"));
+            clone.setLevel(StringUtils.defaultIfBlank(question.getLevel(), "无"));
+            clone.setState(StringUtils.defaultIfBlank(question.getState(), "无"));
+            clone.setQuestionCode(setCodeMark(StringUtils.defaultIfBlank(question.getQuestionCode(), "无")));
+            clone.setBetterCode(setCodeMark(StringUtils.defaultIfBlank(question.getBetterCode(), "无")));
+            clone.setDesc(StringUtils.defaultIfBlank(question.getDesc(), "无"));
             questionList.add(clone);
         }
         return questionList;
@@ -106,6 +105,6 @@ public class CrQuestionUtils {
             str = ">>" + str + "\n";
             result.append(str);
         }
-        return result.toString();
+        return result.toString().trim();
     }
 }
