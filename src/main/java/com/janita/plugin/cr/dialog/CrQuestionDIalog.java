989bbe058b5d5674523b63ede39ae6d13a4afd33
zhucj <zhucj@servyou.com.cn>
2022-04-15 10:17:24 +0800

    模版

---
+++ src/main/java/com/janita/plugin/cr/dialog/CrQuestionDIalog.java
@@ -0,0 +1,42 @@
+package com.janita.plugin.cr.dialog;
+
+import javax.swing.*;
+
+/**
+ * CrQuestionDIalog
+ *
+ * @author zhucj
+ * @since 20220324
+ */
+public class CrQuestionDIalog {
+
+    private JPanel contentPanel;
+
+    private JPanel 顶部区块;
+
+    private JPanel 中间区块;
+
+    private JPanel 底部区块;
+
+    private JPanel 顶部左边;
+
+    private JPanel 顶部右边;
+
+    private JComboBox comboBox1;
+
+    private JComboBox comboBox2;
+
+    private JComboBox comboBox3;
+
+    private JComboBox comboBox4;
+
+    private JButton button1;
+
+    private JButton button2;
+
+    private JButton button3;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
