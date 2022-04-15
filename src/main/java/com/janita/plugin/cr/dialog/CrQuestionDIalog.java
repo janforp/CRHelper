fde5f96238ed13d480b3384fcec62bf476623716
zhucj <zhucj@servyou.com.cn>
2022-04-15 10:27:16 +0800

    模版

--- src/main/java/com/janita/plugin/cr/dialog/CrQuestionDIalog.java
+++ src/main/java/com/janita/plugin/cr/dialog/CrQuestionDIalog.java
@@ -12,29 +12,25 @@ public class CrQuestionDIalog {
 
     private JPanel contentPanel;
 
-    private JPanel 顶部区块;
+    private JComboBox questionTypeBox;
 
-    private JPanel 中间区块;
+    private JComboBox levelBox;
 
-    private JPanel 底部区块;
+    private JComboBox toAccountBox;
 
-    private JPanel 顶部左边;
+    private JComboBox stateBox;
 
-    private JPanel 顶部右边;
+    private JButton buttonOk;
 
-    private JComboBox comboBox1;
+    private JButton solveButton;
 
-    private JComboBox comboBox2;
+    private JButton buttonCancel;
 
-    private JComboBox comboBox3;
+    private JScrollPane questionCodeArea;
 
-    private JComboBox comboBox4;
+    private JScrollPane betterCodeArea;
 
-    private JButton button1;
-
-    private JButton button2;
-
-    private JButton button3;
+    private JScrollPane descArea;
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
