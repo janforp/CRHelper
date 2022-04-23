b13a0ec171846884ac800707c0334a867c5be1b8
zhucj <zhucj@servyou.com.cn>
2022-04-23 20:30:25 +0800

    代码优化

--- src/main/java/com/janita/plugin/cr/component/CrQuestionApplication.java
+++ src/main/java/com/janita/plugin/cr/component/CrQuestionApplication.java
@@ -1,6 +1,9 @@
 package com.janita.plugin.cr.component;
 
 import com.intellij.openapi.components.ApplicationComponent;
+import com.janita.plugin.common.enums.CrDataStorageEnum;
+import com.janita.plugin.cr.setting.CrQuestionSetting;
+import com.janita.plugin.db.DatabaseServiceFactory;
 import com.janita.plugin.db.impl.SqliteDatabaseServiceImpl;
 
 /**
@@ -13,8 +16,25 @@ import com.janita.plugin.db.impl.SqliteDatabaseServiceImpl;
 public class CrQuestionApplication implements ApplicationComponent {
 
     @Override
+    public void initComponent() {
+        CrQuestionSetting settingFromCache = CrQuestionSetting.getCrQuestionSettingFromCache();
+        CrDataStorageEnum storageWay = settingFromCache.getStorageWay();
+        if (storageWay == CrDataStorageEnum.MYSQL_DB || storageWay == CrDataStorageEnum.SQLITE_DB) {
+            new Thread(new Task()).start();
+        }
+    }
+
+    @Override
     public void disposeComponent() {
         SqliteDatabaseServiceImpl.getInstance().closeResource();
         ApplicationComponent.super.disposeComponent();
+    }
+
+    private static class Task implements Runnable {
+
+        @Override
+        public void run() {
+            DatabaseServiceFactory.getDatabase();
+        }
     }
 }
