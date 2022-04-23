beacd13718c40f06d16f53b39ddc8e7ed7f24457
zhucj <zhucj@servyou.com.cn>
2022-04-23 20:21:27 +0800

    代码优化

--- src/main/java/com/janita/plugin/cr/component/CrQuestionApplication.java
+++ src/main/java/com/janita/plugin/cr/component/CrQuestionApplication.java
@@ -1,15 +1,7 @@
 package com.janita.plugin.cr.component;
 
 import com.intellij.openapi.components.ApplicationComponent;
-import com.janita.plugin.common.enums.CrDataStorageEnum;
-import com.janita.plugin.cr.dialog.CrQuestionSettingDialog;
-import com.janita.plugin.cr.setting.CrQuestionSetting;
-import com.janita.plugin.db.IDatabaseService;
-import com.janita.plugin.db.impl.AbstractIDatabaseService;
-import com.janita.plugin.db.impl.MySqlDatabaseServiceImpl;
 import com.janita.plugin.db.impl.SqliteDatabaseServiceImpl;
-
-import java.sql.Connection;
 
 /**
  * CrQuestionApplication
@@ -19,22 +11,6 @@ import java.sql.Connection;
  */
 @SuppressWarnings("all")
 public class CrQuestionApplication implements ApplicationComponent {
-
-    @Override
-    public void initComponent() {
-        CrQuestionSetting settingFromCache = CrQuestionSetting.getCrQuestionSettingFromCache();
-        CrDataStorageEnum storageWay = settingFromCache.getStorageWay();
-        if (storageWay == CrDataStorageEnum.MYSQL_DB) {
-            IDatabaseService service = MySqlDatabaseServiceImpl.getInstance();
-            Connection connection = service.getConnection();
-            if (connection == AbstractIDatabaseService.INVALID_CONNECT) {
-                CrQuestionSettingDialog dialog = new CrQuestionSettingDialog();
-                if (dialog.showAndGet()) {
-                    dialog.doOKAction();
-                }
-            }
-        }
-    }
 
     @Override
     public void disposeComponent() {
